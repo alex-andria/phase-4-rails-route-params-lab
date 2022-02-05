1574f46609e6caa115eb4c3e9e48992bf3c3c952
Alexandria DAntonio <dantonio.alexandria@gmail.com>
2022-02-05 16:22:40 -0500

    commit tree e5c41c2276406107cce0d570c9e6d7051620bd5d

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,5 +1,15 @@
 class Student < ApplicationRecord
 
+  # bonus start
+    # class method written using a scope (https://www.rubyguides.com/2019/10/scopes-in-ruby-on-rails/)
+    scope :by_name, ->(name) { where("first_name LIKE ? OR last_name LIKE ?", "%#{name}%", "%#{name}%") }
+  
+    # class method written without using a scope
+    # def self.by_name(name)
+    #   Student.where("first_name LIKE ? OR last_name LIKE ?", "%#{name}%", "%#{name}%")
+    # end
+    
+  # bonus end
   def to_s
     "#{first_name} #{last_name}"
   end
